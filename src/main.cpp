05aa4ffdd1163b71cf7928cc2c9d4f802eadd713
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-09-14 16:00:24 +0200

    Created splines header file

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -101,6 +102,8 @@ int main() {
 
           double dist_inc = 0.5;
           for (int i = 0; i < 50; ++i) {
+
+
             next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
             next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
           }
